73d83d7889bbf635ef86c9c41bb9afda32f9ea0e
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-06-24 16:56:42 -0500

    Added line chart

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-const SAMPLE_BARCHART_DATA: any[] = [
+const BAR_CHART_DATA: any[] = [
   { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
   { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
 ];
 
-const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
+const BAR_CHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
 
 @Component({
   selector: 'app-bar-chart',
@@ -13,8 +13,8 @@ const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W
   styleUrls: ['./bar-chart.component.less']
 })
 export class BarChartComponent implements OnInit {
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartData: any[] = BAR_CHART_DATA;
+  public barChartLabels: string[] = BAR_CHART_LABELS;
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_DATA: any[] = [
+  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+  { data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
+  { data: [52, 34, 49, 53, 68, 62], label: 'Forecasting'}
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -6,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.less']
 })
 export class LineChartComponent implements OnInit {
+
+  lineChartData: any[] = LINE_CHART_DATA;
+  lineChartLabels: string[] = LINE_CHART_LABELS;
+  lineChartType = 'line';
+  lineChartLegend = true;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartColors = LINE_CHART_COLORS;
 
   constructor() { }
 
