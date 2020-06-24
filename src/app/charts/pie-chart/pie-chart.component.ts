aab9a8b8c016391be6ab3e2922c05725c52c6896
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-06-24 00:12:48 -0500

    Added pie/doughnut charts and data

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['XYZ Logistics', 'Main St Bakery', 'Acme Hosting'];
+  pieChartColors: any[] = [
+    {
+      backgroundColor: ['#26547c','#ff6b6b','#ffd166'],
+      borderColor: '#333'
+    }
+  ];
+  pieChartType = 'doughnut';
+  
   ngOnInit(): void {
   }
 
