557ebcb3688f5661aba90c2b5d7b48f6d7eff77a
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-06-25 00:57:18 -0500

    Updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,29 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnLine);
   }
 
+  setServerStatus(isOnLine: boolean) {
+    if (isOnLine){
+      this.serverInput.isOnLine = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+    }
+    else {
+      this.serverInput.isOnLine = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';    }
+  }
+
+  toggleStatus(onLineStatus: boolean) {
+    console.log(this.serverInput.name, ':', onLineStatus);
+    this.setServerStatus(!onLineStatus);
+  }
 }
